e243412e4d2d05119594faab00df3b9f9754c34f
Utsav Singh <utsav.utsavsingh.singh@gmail.com>
2026-04-17 00:51:41 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="bg-black text-white min-h-screen p-6">
+      <h1 className="text-4xl font-bold">🏋️ Fitnara</h1>
+      <p className="text-gray-400 mt-2">
+        Discover the best gyms near you
+      </p>
+    </main>
+  )
+}
