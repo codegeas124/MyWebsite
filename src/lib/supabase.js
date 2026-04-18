07d1610c4e67391f06d08b0c8d64f612e21ce30d
Utsav Singh <utsav.utsavsingh.singh@gmail.com>
2026-04-18 13:02:23 +0530

    Updated Supabase URL and key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-)
+const supabaseUrl = process.env.https://mylwztzuuxhpqrfyowtd.supabase.co
+const supabaseAnonKey = process.env.mylwztzuuxhpqrfyowtd
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
